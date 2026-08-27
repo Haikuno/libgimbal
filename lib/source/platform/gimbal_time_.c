98ca2e28a3bd65e403ff9cec84ad96c7568593a7
Agustín Bellagamba <67402084+Haikuno@users.noreply.github.com>
2026-08-27 01:49:01 -0300

    fix MinGW timespec fallback for Windows XP

--- lib/source/platform/gimbal_time_.c
+++ lib/source/platform/gimbal_time_.c
@@ -2,12 +2,33 @@
 
 #ifdef GBL_NO_TIMESPEC_
 
+#if defined(_WIN32)
+#include <windows.h>
+#endif
+
 int timespec_get(struct timespec* pSpec, int base) {
     if(base != TIME_UTC)
         return 0;
 
+#if defined(_WIN32)
+    FILETIME fileTime;
+    ULARGE_INTEGER ticks;
+    const ULONGLONG windowsToUnixEpoch100ns = 116444736000000000ULL;
+
+    GetSystemTimeAsFileTime(&fileTime);
+    ticks.LowPart = fileTime.dwLowDateTime;
+    ticks.HighPart = fileTime.dwHighDateTime;
+
+    if(ticks.QuadPart < windowsToUnixEpoch100ns)
+        return 0;
+
+    ticks.QuadPart -= windowsToUnixEpoch100ns;
+    pSpec->tv_sec = (time_t)(ticks.QuadPart / 10000000ULL);
+    pSpec->tv_nsec = (long)((ticks.QuadPart % 10000000ULL) * 100ULL);
+#else
     if(clock_gettime(CLOCK_REALTIME, pSpec) == -1)
         return 0;
+#endif
 
     return TIME_UTC;
 }
